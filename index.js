a4a1c81a98da3e05368cdb74af7fbe4549557144
101zh <67253838+101zh@users.noreply.github.com>
2024-12-10 17:35:49 -0800

    test prepopulating time blocks

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+}
+
+var container = document.getElementById("blockcontainer");
+
+var content = `<div class="block">
+    <div class="descrip">DAY</div>
+    <div class="percent">50%</div>
+    <div class="progressbar">
+        <div></div>
+    </div>
+</div>
+`
+
+// fetch('prefab.html')
+//     .then(response => response.text())
+//     .then(textContent => {
+//         content = textContent
+//     })
+//     .catch(error => console.error('Error:', error));
+
+
+for (let index = 0; index < 3; index++) {
+    container.appendChild(createElementFromHTML(content))
+}
+
 
