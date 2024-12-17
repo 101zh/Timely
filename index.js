2f71be8467a3c38011f7152189357f451ce971a2
101zh <67253838+101zh@users.noreply.github.com>
2024-12-17 09:47:50 -0800

    document and organize helper methods

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function createElementFromHTML(htmlString) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-
-    // Change this to div.childNodes to support multiple top-level nodes.
-    return div;
-}
-
 var timeElement = document.getElementById("time")
 var dateElement = document.getElementById("date")
 var container = document.getElementById("blockcontainer");
@@ -51,26 +43,6 @@ var minutesToSeconds = 60
 var secondsInAWeek = 7 * 24 * 60 * 60
 var secondsInADay = 24 * 60 * 60
 
-function givePercentageOfTimePeriod(dateStart, dateEnd, days, hoursPerDay) {
-    var percentage = 1 - (((dateEnd.getTime() - dateStart.getTime()) / 1000) / (days * hoursPerDay * 60 * 60))
-    if (percentage > 1) {
-        return -1
-    }
-
-    return percentage
-}
-
-function calculateTimePercentages() {
-    var timePercentages = []
-    timePercentages[0] = givePercentageOfTimePeriod(dNow, dEndOfYear, 365, 24)
-    timePercentages[1] = givePercentageOfTimePeriod(dNow, dEndOfMonth, list[dNow.getMonth()], 24)
-    timePercentages[2] = givePercentageOfTimePeriod(dNow, dEndOfWeek, 7, 24)
-    timePercentages[3] = givePercentageOfTimePeriod(dNow, dEndOfDay, 1, 24)
-    timePercentages[4] = givePercentageOfTimePeriod(dNow, dEndOfWorkDay, 1, 8)
-
-    return timePercentages
-}
-
 
 // console.log("workDayStartHour"+"="+9+";"+ new Date(Date.now()+(10*365*24*60*60*1000)))
 // document.cookie = "workDayStartHour" + "=" + 9 + ";SameSite=Strict" + ";expires=" + new Date(Date.now() + (10 * 365 * 24 * 60 * 60 * 1000))
@@ -110,3 +82,49 @@ function updatePercentageBars() {
     }
 }
 
+
+// ----------------------Helper Methods----------------------
+
+/**
+ * 
+ * @param {Date} dateNow The current time as a date object
+ * @param {Date} dateEnd The end time of the time period as a date object
+ * @param {number} days The number of days in the time period
+ * @param {number} hours the number of hours in the time period if `days==1`
+ * @returns {number} The percentage of the time period the current time is at as a decimal
+ */
+function givePercentageOfTimePeriod(dateNow, dateEnd, days, hours) {
+    var percentage = 1 - (((dateEnd.getTime() - dateNow.getTime()) / 1000) / (days * hours * 60 * 60))
+    if (percentage > 1) {
+        return -1
+    }
+
+    return percentage
+}
+
+/**
+ * 
+ * @returns {number[]} An array of time percentages based on the currrent time
+ */
+function calculateTimePercentages() {
+    var timePercentages = []
+    timePercentages[0] = givePercentageOfTimePeriod(dNow, dEndOfYear, 365, 24)
+    timePercentages[1] = givePercentageOfTimePeriod(dNow, dEndOfMonth, list[dNow.getMonth()], 24)
+    timePercentages[2] = givePercentageOfTimePeriod(dNow, dEndOfWeek, 7, 24)
+    timePercentages[3] = givePercentageOfTimePeriod(dNow, dEndOfDay, 1, 24)
+    timePercentages[4] = givePercentageOfTimePeriod(dNow, dEndOfWorkDay, 1, 8)
+
+    return timePercentages
+}
+
+/**
+ * 
+ * @param {String} htmlString The string of the div in html
+ * @returns {HTMLDivElement} an HTMLDivElement created from `htmlString`
+ */
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    return div;
+}
